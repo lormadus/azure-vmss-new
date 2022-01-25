ad28bbf29f2d91018d7c9e9d0e2c635fa8c6c14f
David Yoon <david.hyeshik@yahoo.com>
2022-01-25 11:32:41 +0900

    Update lb_probe.tf

--- lb_probe.tf
+++ lb_probe.tf
@@ -1,6 +1,6 @@
 resource "azurerm_lb_probe" "user30-lb-probe" {
     resource_group_name 	= var.rg
-    loadbalancer_id 		= azurerm_lb.user01-lb.id
+    loadbalancer_id 		= azurerm_lb.user30-lb.id
     name 			= "http-probe"
     protocol 			= "Http"
     request_path 		= "/"
