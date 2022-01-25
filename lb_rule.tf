683403649696b8189b6b7b30ab7fb655a151473a
David Yoon <david.hyeshik@yahoo.com>
2022-01-25 11:18:47 +0900

    Update lb_rule.tf

--- lb_rule.tf
+++ lb_rule.tf
@@ -5,6 +5,7 @@ resource "azurerm_lb_rule" "user30-lb-rule" {
     protocol 				= "Tcp"
     frontend_port 			= 80
     backend_port 			= 80
-    #backend_address_pool_id        	= azurerm_lb_backend_address_pool.user30-bep.id
+    backend_address_pool_id        	= azurerm_lb_backend_address_pool.user30-bep.id
     frontend_ip_configuration_name 	= var.lb_pip
+    probe_id                       = azurerm_lb_probe.user30-lb-probe.id
 }
